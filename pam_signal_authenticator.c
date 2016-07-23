f5af285c2ccd79f9c1dceb54c3ffd7b8298754fc
James Murphy <james.murphy.debian@gmail.com>
2016-07-23 06:41:17 +0000

    Use PAM_EXTERN as specified in the man page

--- pam_signal_authenticator.c
+++ pam_signal_authenticator.c
@@ -363,7 +363,7 @@ int send_signal_msg_and_wait_for_response(pam_handle_t *pamh,
 
 // This is the entry point, think of it as main()
 /* PAM entry point for authentication verification */
-int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     int ret;
 
     bool nullok = !(flags & PAM_DISALLOW_NULL_AUTHTOK);
@@ -497,17 +497,17 @@ int pam_sm_authenticate(pam_handle_t *pamh, int flags, int argc, const char **ar
 
 /* PAM entry point for session creation */
 
-int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return PAM_IGNORE;
 }
 
 /* PAM entry point for session cleanup */
-int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return PAM_IGNORE;
 }
 
 /* PAM entry point for accounting */
-int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return PAM_IGNORE;
 }
 
@@ -515,12 +515,12 @@ int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv)
  PAM entry point for setting user credentials (that is, to actually
  establish the authenticated user's credentials to the service provider)
 */
-int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_setcred(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return PAM_IGNORE;
 }
 
 /* PAM entry point for authentication token (password) changes */
-int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
+PAM_EXTERN int pam_sm_chauthtok(pam_handle_t *pamh, int flags, int argc, const char **argv) {
     return PAM_IGNORE;
 }
 
